6c5cfc900107865aef467db5b299526ef7349a87
Wilson hernandez martinez <wilsonhermar@gmail.com>
2020-12-14 22:19:21 -0500

    Cambiando y cambiando

--- src/router.js
+++ src/router.js
@@ -1,12 +1,12 @@
 import vueRouter from 'vue-router'
 
 import App from './App'
-import Inicio from './components/inicio'
-import Verificacion from './components/verificacion'
-import Buscar from './components/buscar'
-import Actualizar from './components/actualizar'
-import Borrar from './components/borrar'
-import Crear from './components/crear'
+import Inicio from './components/Inicio'
+import Verificacion from './components/Verificacion'
+import Buscar from './components/Buscar'
+import Actualizar from './components/Actualizar'
+import Borrar from './components/Borrar'
+import Crear from './components/Crear'
 
 const router = new vueRouter({
   mode: 'history',
