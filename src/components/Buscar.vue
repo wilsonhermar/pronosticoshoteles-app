4854072254a8bb3e4ff4cecd722ee90f68eaf497
Wilson hernandez martinez <wilsonhermar@gmail.com>
2020-12-15 05:06:54 -0500

    dfdgg

--- src/components/Buscar.vue
+++ src/components/Buscar.vue
@@ -24,7 +24,7 @@ export default {
             let self = this
             let nombre_hotel = null
             //let aux = "https://hotel-pronostico.herokuapp.com/hotel/search/{nombre}?name=" --> URL GITHUB
-            let aux = "https://pronosticohoteles-api.herokuapp.com/{nombre}?name="
+            let aux = "https://pronosticohoteles-api.herokuapp.com/hotel/search/{nombre}?name="
             nombre_hotel = document.getElementsByName("nombreHotel")[0].value
             let url = aux+nombre_hotel
             axios.get(url)
